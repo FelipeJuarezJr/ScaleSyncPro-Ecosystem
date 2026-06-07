52938b237176c185e538c1b424d45178f5fee227
Felipe Juarez Jr. <your-email@example.com>
2026-06-06 19:41:18 -0700

    Firebase config update

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -43,40 +43,43 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // ReptiGram Firebase Configuration
+  // ScaleSync Pro Firebase Configuration
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDiT-1kdubTNYLe2waeCIYvGDx5nakKyh0',
-    appId: '1:373955522567:web:7163187c33d378455bbaa2',
-    messagingSenderId: '373955522567',
-    projectId: 'reptigramfirestore',
-    authDomain: 'reptigramfirestore.firebaseapp.com',
-    storageBucket: 'reptigramfirestore.firebasestorage.app',
-    measurementId: 'G-H7FDWLXW64',
+    apiKey: 'AIzaSyCgE2e8RPN0JOWR8MFA1HTfDos3qmmkmEg',
+    appId: '1:123833527982:web:36b21425fc1127c290473f',
+    messagingSenderId: '123833527982',
+    projectId: 'scalesync-pro',
+    authDomain: 'scalesync-pro.firebaseapp.com',
+    storageBucket: 'scalesync-pro.firebasestorage.app',
+    measurementId: 'G-9BCWD33SRY',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDiT-1kdubTNYLe2waeCIYvGDx5nakKyh0',
-    appId: '1:373955522567:android:7163187c33d378455bbaa2',
-    messagingSenderId: '373955522567',
-    projectId: 'reptigramfirestore',
-    storageBucket: 'reptigramfirestore.firebasestorage.app',
+    apiKey: 'AIzaSyCgE2e8RPN0JOWR8MFA1HTfDos3qmmkmEg',
+    appId: '1:123833527982:android:36b21425fc1127c290473f',
+    messagingSenderId: '123833527982',
+    projectId: 'scalesync-pro',
+    authDomain: 'scalesync-pro.firebaseapp.com',
+    storageBucket: 'scalesync-pro.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDiT-1kdubTNYLe2waeCIYvGDx5nakKyh0',
-    appId: '1:373955522567:ios:7163187c33d378455bbaa2',
-    messagingSenderId: '373955522567',
-    projectId: 'reptigramfirestore',
-    storageBucket: 'reptigramfirestore.firebasestorage.app',
-    iosBundleId: 'com.reptigram.app',
+    apiKey: 'AIzaSyCgE2e8RPN0JOWR8MFA1HTfDos3qmmkmEg',
+    appId: '1:123833527982:ios:36b21425fc1127c290473f',
+    messagingSenderId: '123833527982',
+    projectId: 'scalesync-pro',
+    authDomain: 'scalesync-pro.firebaseapp.com',
+    storageBucket: 'scalesync-pro.firebasestorage.app',
+    iosBundleId: 'com.scalesyncpro.app',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyDiT-1kdubTNYLe2waeCIYvGDx5nakKyh0',
-    appId: '1:373955522567:macos:7163187c33d378455bbaa2',
-    messagingSenderId: '373955522567',
-    projectId: 'reptigramfirestore',
-    storageBucket: 'reptigramfirestore.firebasestorage.app',
-    iosBundleId: 'com.reptigram.app',
+    apiKey: 'AIzaSyCgE2e8RPN0JOWR8MFA1HTfDos3qmmkmEg',
+    appId: '1:123833527982:macos:36b21425fc1127c290473f',
+    messagingSenderId: '123833527982',
+    projectId: 'scalesync-pro',
+    authDomain: 'scalesync-pro.firebaseapp.com',
+    storageBucket: 'scalesync-pro.firebasestorage.app',
+    iosBundleId: 'com.scalesyncpro.app',
   );
 } 
